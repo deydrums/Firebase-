76ce191e810d280c4f91930f8e6364ec9aa1ff31
deydrums <dagarcia100@gmail.com>
2021-07-24 15:40:19 -0600

    Actualizar / Borrar

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,41 @@ const usuario = {
     fechaNaci: 0
 }
 
-//Insert in to usuarios
+const usuariosRef = db.collection('usuarios');
+
+//INSERTAR REGISTRO
 
 // db.collection('usuarios')
 //     .add(usuario)
 //     .then(docRef =>{
 //         console.log(docRef.delete())
 //     })
+//     .catch(e => console.log('error',e));
+
+//ACTUALIZAR REGISTRO
+
+//update 
+
+// usuariosRef
+//     .doc('9VAzZUp6ICMFJdS7W0JS')
+//     .update({
+//         activo: true
+//     });
+
+//destructivo
+
+// usuariosRef
+// .doc('9VAzZUp6ICMFJdS7W0JS')
+// .set({
+//     activo: true,
+//     edad: 44,
+// });
+
+
+//DELETE REGISTRO WHERE
+
+// usuariosRef
+//     .doc('nOJZKQHnLCB3gIj0Lpxt')
+//     .delete()
+//     .then(() => console.log('Borrado'))
 //     .catch(e => console.log('error',e));
